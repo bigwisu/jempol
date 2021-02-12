9e6da40cd88807ddf34c62aa502207dd77705190
Wisu Suntoyo <wisu@google.com>
2021-02-12 20:15:04 +0700

    photo interface

--- src/app/interfaces.ts
+++ src/app/interfaces.ts
@@ -2,7 +2,7 @@ export interface Photo {
   code: string;
   caption: string;
   likes: number;
-  boos: number;
+  hates: number;
   display_src: string;
   id: string;
 }
--- src/app/mock-photos.ts
+++ src/app/mock-photos.ts
@@ -5,7 +5,7 @@ export const PHOTOS: Photo[] = [
         code: 'BAcyDyQwcXX',
         caption: 'Ingat.. sehat itu asik!',
         likes: 56,
-        boos: 15,
+        hates: 15,
         id: '1161022966406956503',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550244134/renee-fisher-1106240.jpg'
       },
@@ -13,7 +13,7 @@ export const PHOTOS: Photo[] = [
         code: 'BAcJeJrQca9',
         caption: 'Ceasar salad... yay!',
         likes: 59,
-        boos: 2,
+        hates: 2,
         id: '1160844458347054781',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550244130/taylor-kiser-373480.jpg'
       },
@@ -21,7 +21,7 @@ export const PHOTOS: Photo[] = [
         code: 'BAF_KY4wcRY',
         caption: 'Deadlift... salah satu olahraga yang mengikut-sertakan sangat bangian otot tubuh',
         likes: 79,
-        boos: 3,
+        hates: 3,
         id: '1154606670337393752',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550244127/victor-freitas-546968.jpg'
       },
@@ -29,7 +29,7 @@ export const PHOTOS: Photo[] = [
         code: 'BAPIPRjQce9',
         caption: 'Kettlebell!!',
         likes: 47,
-        boos: 4,
+        hates: 4,
         id: '1157179863266871229',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550244125/maria-fernanda-gonzalez-461523.jpg'
       },
@@ -37,7 +37,7 @@ export const PHOTOS: Photo[] = [
         code: '-hZh6IQcfN',
         caption: 'Semangkuk oatmeal chantique...',
         likes: 66,
-        boos: 7,
+        hates: 7,
         id: '1126293663140399053',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550244118/sambazon-63913.jpg'
       },
@@ -45,7 +45,7 @@ export const PHOTOS: Photo[] = [
         code: '-B3eiIwcYV',
         caption: 'Widiwww... rumah Spongebob!',
         likes: 33,
-        boos: 3,
+        hates: 3,
         id: '1117418173361145365',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550244111/pineapple-supply-co-29325.jpg'
       },
@@ -53,7 +53,7 @@ export const PHOTOS: Photo[] = [
         code: 'BAhvZrRwcfu',
         caption: 'Salad lagi neh.. jangan bosan, sebab salad akan cukup mengenyangkan dengan sedikit sekali calori yang anda konsumsi',
         likes: 30,
-        boos: 9,
+        hates: 9,
         id: '1162418651480049646',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550244111/sara-dubler-769470.jpg'
       },
@@ -61,7 +61,7 @@ export const PHOTOS: Photo[] = [
         code: 'BAAJqbOQcW5',
         caption: 'Buah naga! Sangat banyak mengandung vitamin dan antioksidan...',
         likes: 40,
-        boos: 7,
+        hates: 7,
         id: '1152964002473690553',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550244096/pontus-ohlsson-422759.jpg'
       },
@@ -69,7 +69,7 @@ export const PHOTOS: Photo[] = [
         code: '_4jHytwcUA',
         caption: 'Jangan lupa cardio!',
         likes: 62,
-        boos: 5,
+        hates: 5,
         id: '1150824171912152320',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550244064/luke-chesser-22256.jpg'
       },
@@ -77,7 +77,7 @@ export const PHOTOS: Photo[] = [
         code: '_zbaOlQcbn',
         caption: 'Jangan lupa untuk selalu monitor progress terutama lingkar pinggang kamu',
         likes: 52,
-        boos: 2,
+        hates: 2,
         id: '1149382879529256679',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550244044/rawpixel-1054662.jpg'
       },
@@ -85,7 +85,7 @@ export const PHOTOS: Photo[] = [
         code: '_rmvQfQce8',
         caption: 'Telor adalah sumber protein yang sangat mudah didapatkan...',
         likes: 35,
-        boos: 1,
+        hates: 1,
         id: '1147180903383025596',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550244035/joseph-gonzalez-176749.jpg'
       },
@@ -93,7 +93,7 @@ export const PHOTOS: Photo[] = [
         code: '_ep9kiQcVy',
         caption: 'Memulai hari dengan semangkuk oatmeal dengan topping yang menggoda... 🍜',
         likes: 64,
-        boos: 2,
+        hates: 2,
         id: '1143535906423162226',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550244022/jannis-brandt-256862.jpg'
       },
@@ -101,7 +101,7 @@ export const PHOTOS: Photo[] = [
         code: '_XpJcrwcSn',
         caption: 'Kecenderungannya makanan sehat selalu kelihatan cantik!',
         likes: 74,
-        boos: 12,
+        hates: 12,
         id: '1141561999742846119',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550244017/kevin-mccutcheon-665788.jpg'
       },
@@ -109,7 +109,7 @@ export const PHOTOS: Photo[] = [
         code: '_KnU7MwceA',
         caption: 'Selain olahraga beban rutin... pola makan harus senantiasa dijaga',
         likes: 54,
-        boos: 2,
+        hates: 2,
         id: '1137894817632733056',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550244008/edgar-castrejon-459824.jpg'
       },
@@ -117,7 +117,7 @@ export const PHOTOS: Photo[] = [
         code: '_HMejJQcY5',
         caption: 'Bali, sunset di pantai Canggu... keren banget!',
         likes: 18,
-        boos: 5,
+        hates: 5,
         id: '1136932306813044281',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550243995/dominik-vanyi-435737.jpg'
       },
@@ -125,7 +125,7 @@ export const PHOTOS: Photo[] = [
         code: '_Fq2zmwcaz',
         caption: 'Awesome....',
         likes: 48,
-        boos: 4,
+        hates: 4,
         id: '1136502965197194931',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550243958/danielle-cerullo-782570.jpg'
       },
@@ -133,7 +133,7 @@ export const PHOTOS: Photo[] = [
         code: '-1rhFawccs',
         caption: 'Beban.. beban.. beban... mana yang mau dipilih',
         likes: 39,
-        boos: 7,
+        hates: 7,
         id: '1132002270913873708',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550278131/luis-reyes-1204032.jpg'
       },
@@ -141,7 +141,7 @@ export const PHOTOS: Photo[] = [
         code: '-pjx-gQcVi',
         caption: 'Jangan takut untuk menaklukkan beban yang semakin tinggi... ingat otot anda yang ngebakar lemak!',
         likes: 81,
-        boos: 2,
+        hates: 2,
         id: '1128590547628442978',
         display_src: 'https://res.cloudinary.com/asiksehat/image/upload/c_thumb,h_540,w_540/v1550278129/victor-freitas-549214.jpg'
       }
